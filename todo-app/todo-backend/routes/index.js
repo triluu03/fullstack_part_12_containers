cc69e679b72e83b9f4c87ec23df6714704ba0a79
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2022-10-29 21:08:28 +0300

    Exercise 12.10

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const redis = require('../redis');
+const { getAsync } = require('../redis');
 const router = express.Router();
 
 const configs = require('../util/config');
@@ -13,6 +13,14 @@ router.get('/', async (req, res) => {
   res.send({
     ...configs,
     visits,
+  });
+});
+
+// GET statistics data
+router.get('/statistics', async (req, res) => {
+  const addedTodos = await getAsync('added_todos');
+  res.send({
+    addedTodos: JSON.parse(addedTodos),
   });
 });
 
--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { Todo } = require('../mongo');
+const { getAsync, setAsync } = require('../redis');
 const router = express.Router();
 
 /* GET todos listing. */
@@ -14,6 +15,8 @@ router.post('/', async (req, res) => {
     text: req.body.text,
     done: false,
   });
+  const added_todos = await getAsync('added_todos');
+  setAsync('added_todos', added_todos === null ? 1 : added_todos + 1);
   res.send(todo);
 });
 
